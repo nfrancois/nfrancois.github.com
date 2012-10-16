9c463e1a0a98a636d782a8d2f89d3b80779344f8
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2012-10-16 13:55:09 +0200

    fix title

--- balle/Balle.dart
+++ balle/Balle.dart
@@ -1,4 +1,5 @@
 #import('dart:html');
+#import('dart:math');
 
 CanvasRenderingContext2D ctx;
 
@@ -18,15 +19,15 @@ bool animate(int time){
 void draw() {
 
   var time = new Date.now().millisecondsSinceEpoch * 0.002;
-  var x = Math.sin( time ) * 96 + 128;
-  var y = Math.cos( time * 0.9 ) * 96 + 128;
+  var x = sin( time ) * 96 + 128;
+  var y = cos( time * 0.9 ) * 96 + 128;
 
   ctx.fillStyle = 'rgb(245,245,245)';
   ctx.fillRect( 0, 0, 255, 255 );
 
   ctx.fillStyle = 'rgb(255,0,0)';
   ctx.beginPath();
-  ctx.arc( x, y, 10, 0, Math.PI * 2, true );
+  ctx.arc( x, y, 10, 0, PI * 2, true );
   ctx.closePath();
   ctx.fill();
 
